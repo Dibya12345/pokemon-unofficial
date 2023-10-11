42c07c73bf5c1acdeccb0fe34fa9210626010c0e
Dibya12345 <48878439+Dibya12345@users.noreply.github.com>
2023-10-11 16:30:03 +0000

    added pokemon class

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log("Game started")
+class Pokemon{
+    constructor(name, health, type){
+        this.name = name
+        this.health = health
+        this.type = type
+        this.abilities = [];
+    }
+}
+
+const bulbasaur = new Pokemon("bulb", 12, 45)
+bulbasaur.abilities.push({"mericham": 30})
+console.log(bulbasaur)
